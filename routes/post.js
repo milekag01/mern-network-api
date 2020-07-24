123b93346af859cc7297f30e6d9867d3f70b6acb
milek agrawal <milekag01@gmail.com>
2020-07-24 10:49:16 +0530

    add post methods

--- routes/post.js
+++ routes/post.js
@@ -1,5 +1,13 @@
 const express = require("express");
-const { getPosts, createPost } = require("../controllers/post");
+const {
+    getPosts,
+    createPost,
+    postsByUser,
+    postById,
+    isPoster,
+    deletePost,
+    updatePost
+} = require("../controllers/post");
 const { requireSignin } = require("../controllers/auth");
 const { userById } = require("../controllers/user");
 const { createPostValidator } = require("../validator");
@@ -7,9 +15,19 @@ const { createPostValidator } = require("../validator");
 const router = express.Router();
 
 router.get("/", getPosts);
-router.post("/post", requireSignin, createPostValidator, createPost);
+router.post(
+    "/post/new/:userId",
+    requireSignin,
+    createPost,
+    createPostValidator
+);
+router.get("/posts/by/:userId", requireSignin, postsByUser);
+router.put("/post/:postId", requireSignin, isPoster, updatePost);
+router.delete("/post/:postId", requireSignin, isPoster, deletePost);
 
-// any route containing :userId, app will first execute userByID()
+// any route containing :userId, app will first execute userById()
 router.param("userId", userById);
+// any route containing :postId, app will first execute postById()
+router.param("postId", postById);
 
 module.exports = router;
