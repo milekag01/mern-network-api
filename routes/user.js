d5608a1153f6cd50c782e2cf1278c9af26bcc82e
milek agrawal <milekag01@gmail.com>
2020-07-26 05:44:49 +0530

    add seperate route for user photo

--- routes/user.js
+++ routes/user.js
@@ -4,17 +4,18 @@ const {
     allUsers,
     getUser,
     updateUser,
-    deleteUser
+    deleteUser,
+    userPhoto
 } = require("../controllers/user");
 const { requireSignin } = require("../controllers/auth");
-const { hasAuthorization } = require("../controllers/user");
 
 const router = express.Router();
 
 router.get("/users", allUsers);
 router.get("/user/:userId", requireSignin, getUser);
-router.put("/user/:userId",  requireSignin, hasAuthorization, updateUser);
-router.delete("/user/:userId", requireSignin, deleteUser);
+router.put("/user/:userId", requireSignin, updateUser);
+// photo
+router.get("/user/photo/:userId", userPhoto);
 
 // any route containing :userId, our app will first execute userByID()
 router.param("userId", userById);
