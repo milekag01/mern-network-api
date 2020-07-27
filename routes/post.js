cfd495d6b3604e4b242a69f8850535408e3ac259
milek agrawal <milekag01@gmail.com>
2020-07-27 10:11:37 +0530

    add comment uncomment controller

--- routes/post.js
+++ routes/post.js
@@ -10,7 +10,9 @@ const {
     photo,
     singlePost,
     like,
-    unlike
+    unlike,
+    comment,
+    uncomment
 } = require("../controllers/post");
 const { requireSignin } = require("../controllers/auth");
 const { userById } = require("../controllers/user");
@@ -22,6 +24,10 @@ router.get("/posts", getPosts);
 // like unlike
 router.put("/post/like", requireSignin, like);
 router.put("/post/unlike", requireSignin, unlike);
+
+// comments
+router.put("/post/comment", requireSignin, comment);
+router.put("/post/uncomment", requireSignin, uncomment);
 
 router.post(
     "/post/new/:userId",
